fbaf65cd65043625cccaddaab78144a88bc25109
vitor.calegari <vitorprca@gmail.com>
2023-04-25 11:18:20 -0300

    criando as classes derivadas da classe Bot

---
+++ Bots/BotFeliz.py
@@ -0,0 +1,36 @@
+from Bots.Bot import Bot
+
+class BotFeliz(Bot):
+    def __init__(self, nome):
+        self.__nome = nome
+        super().__comandos = {
+            1: ["Bom dia", "Ótimo dia!!!"],
+            2: ["Qual o seu nome?", f"Olá, me chamo {self.__nome} e estou aqui para te ajudar!"],
+            3: ["Quero um conselho", "Seja feliz e viva a vida alegramente! :)"],
+            4: ["Adeus", "Poxa! Já está indo? Que pena..."]
+            }
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    def apresentacao(self):
+        print(f"Aaaargh. Meu nome é {self.__nome} e eu te odeio!(Mais que o Yoda)")
+    
+    def boas_vindas(self):
+        boas_vindas = "Tanto bot pra ser escolhido e tu me escolhe???"
+        print(boas_vindas)
+
+    def despedida(self):
+        despedida = "Graças a Deus!"
+        print(despedida)
+
+    def mostra_comandos(self):
+        pass
+    
+    def executa_comando(self,cmd):
+        pass
--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -1,32 +1,33 @@
 from Bots.Bot import Bot
 
 class BotZangado(Bot):
-    def __init__(self, nome_usuario):
-        self.__nome = nome_usuario
-        self.__comandos = {1: 'Para quem?', 2: 'Silêncio', 3: 'Pensa com a mente que você mesmo tem um', 4: 'Te manca'}
+    def __init__(self, nome):
+        self.__nome = nome
+        super().__comandos = {
+            1: ["Bom dia", 'Para quem?'], 
+            2: ["Qual o seu nome?", 'Silêncio.'], 
+            3: ["Quero um conselho", 'Pensa com a mente que você mesmo tem um.'], 
+            4: ["Adeus", 'Te manca.']}
 
     @property
     def nome(self):
-        return nome
+        return self.__nome
 
     @nome.setter
-    def nome(self, nome_usuario):
-        self.nome = nome_usuario
+    def nome(self, nome):
+        self.__nome = nome
 
     def apresentacao(self):
-        print("Grrrrr. Meu nome é Yoda e eu te odeio!")
- 
-    def mostra_comandos(self):
-        print("1 - Bom dia\n2 - Qual o seu nome?\n3 - Quero um conselho\n4 - Adeus")
-    
-    def executa_comando(self,cmd):
-        print(self.__comandos[1])
-        print(self.__comandos[2])
-        print(self.__comandos[3])
-        print(self.__comandos[4])
+        print(f"Grrrrr. Meu nome é {self.__nome} e eu te odeio!")
 
     def boas_vindas(self):
         print("Ótimo te ver por aqui... mas já pode ir embora!")
 
     def despedida(self):
         print("Já vai tarde.")
+
+    def executa_comando(self,cmd):
+        pass
+
+    def mostra_comandos(self):
+        pass
