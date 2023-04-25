19a0a2d1bfd84edeb3e68d566264f56a68184db1
vitor.calegari <vitorprca@gmail.com>
2023-04-25 11:19:51 -0300

    Completou a classe e adiciona métodos de executar e mostrar comandos

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -5,24 +5,32 @@ import random as r
 
 class Bot(ABC):
 
-    def __init__(nome,):
-        self.nome = nome
-        self.comandos = {}
+    def __init__(self, nome, comandos):
+        self.__nome = nome
+        self.__comandos = {}
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def comandos(self):
+        return self.__comandos
+    
+    @comandos.setter
+    def comandos(self, comandos):
+        self.__comandos = comandos
 
     def mostra_comandos(self):
-        pass
-
-    @abstractmethod
+        for chave, valor in self.__comandos.items():
+            print(f'{chave} - {valor[0]}')
+        
     def executa_comando(self,cmd):
-        pass
+        print(self.__comandos[cmd][1])
 
     @abstractmethod
     def boas_vindas():
