3a24745b78a81779d8054723c4184b22b5f6169e
vitor.calegari <vitorprca@gmail.com>
2023-04-25 11:41:48 -0300

    Adiciona bot feliz a lista de bots

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #encoding: utf-8
+from Bots.BotFeliz import BotFeliz
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotZangado("Yoda"), BotFeliz('Feliz')]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
