af292c4a99d448dc73389b282dc9c784bd9d7ea2
Vitor Calegari <vitorprca@gmail.com>
2023-04-27 16:29:05 -0300

    Adiciona um novo bot: Vex, um objeto do tipo 'BotTriste'

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #encoding: utf-8
 from Bots.BotFeliz import BotFeliz
-from SistemaChatBot import SistemaChatBot as scb
+from Bots.BotTriste import BotTriste
 from Bots.BotZangado import BotZangado
+from SistemaChatBot import SistemaChatBot as scb
+
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotFeliz('Feliz')]
+lista_bots = [BotZangado("Yoda"), BotFeliz("Feliz"), BotTriste("Vex")]
 
 sys = scb.SistemaChatBot("CrazyBots", lista_bots)
 sys.inicio()
