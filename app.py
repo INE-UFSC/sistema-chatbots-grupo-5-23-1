91eb9f3412eb14d9f4d040f7a32db6187db41ac4
Vitor Calegari <vitorprca@gmail.com>
2023-04-27 22:32:37 -0300

    Adiciona BotFeliz

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from Bots.BotFeliz import BotFeliz
 from Bots.BotTriste import BotTriste
 from Bots.BotZangado import BotZangado
 from SistemaChatBot import SistemaChatBot as scb
-
+from BotsExternos.BotFelizGrupo3 import BotFelizGrupo3
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotFeliz("Feliz"), BotTriste("Vex")]
+lista_bots = [BotZangado("Yoda"), BotFeliz("Feliz"), BotTriste("Vex"), BotFelizGrupo3("Rita")]
 
 sys = scb.SistemaChatBot("CrazyBots", lista_bots)
 sys.inicio()
