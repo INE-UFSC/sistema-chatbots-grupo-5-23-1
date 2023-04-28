67087ffcbfb5fa6655d6d1343b7fb9f8c8cc7306
Vitor Calegari <vitorprca@gmail.com>
2023-04-27 22:36:30 -0300

    Adiciona bot do grupo 3

---
+++ BotsExternos/BotGrupo3.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from random import randint
+
+class BotGrupo3(ABC):
+    def __init__(self, nome):
+        self.__nome = nome
+        self._comandos = {}
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    def mostra_comandos(self):
+        cmds = []
+        for comando, valor in self._comandos.items():
+            cmds.append(f'{comando}: {valor}')
+        return '\n'.join(cmds)
+
+    @abstractmethod
+    def apresentacao(self):
+        pass
+
+    @abstractmethod
+    def boas_vindas(self):
+        pass
+
+    @abstractmethod
+    def executa_comando(self, cmd):
+        pass
+
+    @abstractmethod
+    def despedida():
+        pass
