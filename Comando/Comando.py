773f70f62822d9a2ba821492fa9417f3917b3cf8
roberto.becker <robertobeckerbt@gmail.com>
2023-05-05 10:04:31 -0300

    modelagem da classe Comando

---
+++ Comando/Comando.py
@@ -0,0 +1,24 @@
+class Comando:
+    # recebe o id (inteiro), a mensagem e as respostas (opcional)
+    def __init__(self, id, msg, respostas = []):
+        pass
+
+    # get id
+    def id(self):
+        pass
+
+    # get mensagem
+    def mensagem(self):
+        pass 
+
+    # retorna uma resposta aleatória
+    def getRandomResposta(self):
+        pass
+    
+    # adiciona resposta
+    def addResposta(self, resposta):
+        pass
+
+    # remove resposta (opcional)
+    def delResposta(self, resposta):
+        pass
